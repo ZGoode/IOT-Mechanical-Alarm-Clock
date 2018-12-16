5f3f5ff27c5fba3a6086bd71cac6a941433d1e29
Zachary Nathan Goode <zachary@the-goodes.com>
2018-12-16 12:15:46 -0800

    update

---
+++ src/IOT-Mechanical-Alarm-Clock/Html.h
@@ -0,0 +1,84 @@
+String parseHomePage();
+String parseConfigurePage();
+
+String footer = "<!-- Footer -->"
+                "<footer class='w3-bottom w3-center w3-black w3-padding-small w3-opacity w3-hover-opacity-off'>"
+                "<div class='w3-xlarge'>"
+                "<a href='https://github.com/ZGoode/IOT-Mechanical-Alarm-Clock'><i class='fa fa-github w3-hover-opacity'></i></a>"
+                "<a href='https://twitter.com/FlamingBandaid'><i class='fa fa-twitter w3-hover-opacity'></i></a>"
+                "<a href='http://linkedin.com/in/zachary-goode-724441160'><i class='fa fa-linkedin w3-hover-opacity'></i></a>"
+                "</div>"
+                "</footer>"
+                ""
+                "</body>"
+                "</html>";
+
+String header = "<!DOCTYPE html>"
+                "<html>"
+                "<title>IOT Mechanical Alarm Clock</title>"
+                "<meta charset='UTF-8'>"
+                "<meta name='viewport' content='width=device-width, initial-scale=1'>"
+                "<link rel='stylesheet' href='https://www.w3schools.com/w3css/4/w3.css'>"
+                "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Lato'>"
+                "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>"
+                "<style>"
+                "body,h1,h2,h3,h4,h5,h6 {font-family: 'Lato', sans-serif;}"
+                "body, html {"
+                "height: 100%;"
+                "color: #777;"
+                "line-height: 1.8;"
+                "}"
+                "/* Create a Parallax Effect */"
+                ".bgimg{"
+                "background-attachment: fixed;"
+                "background-position: center;"
+                "background-repeat: no-repeat;"
+                "background-size: cover;"
+                "}"
+                "/* Background Picture */"
+                ".bgimg{"
+                "background-image: url('%BACKGROUND_IMAGE%');"
+                "min-height: 100%;"
+                "}"
+                ".w3-wide {letter-spacing: 10px;}"
+                ".w3-hover-opacity {cursor: pointer;}"
+                "</style>"
+                "<body>"
+                "<!-- Navbar (sit on top) -->"
+                "<div class='w3-top'>"
+                "<div class='w3-bar' id='myNavbar'>"
+                "<a href='Home' class='w3-bar-item w3-button'>HOME</a>"
+                "<a href='Configure' class='w3-bar-item w3-button w3-hide-small'><i class='fa fa-cogs'></i> CONFIGURE</a>"
+                "<a href='https://github.com/ZGoode/IOT-Mechanical-Alarm-Clock' class='w3-bar-item w3-button w3-hide-small'><i class='fa fa-th'></i> ABOUT</a>"
+                "<a href='/WifiReset' class='w3-bar-item w3-button w3-hide-small w3-right w3-hover-red'>WIFI RESET</a>"
+                "<a href='/FactoryReset' class='w3-bar-item w3-button w3-hide-small w3-right w3-hover-red'>FACTORY RESET</a>"
+                "</div>"
+                "</div>";
+
+String homePage = "<!-- First Parallax Image with Logo Text -->"
+                  "<div class='bgimg w3-display-container w3-opacity-min' id='home'>"
+                  "<div class='w3-display-middle' style='white-space:nowrap;'>"
+                  "<p><span class='w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity'>IOT<span class='w3-hide-small'> MECHANICAL </span> ALARM</span> CLOCK</span></p>"
+                  "</div>"
+                  "</div>";
+
+String configurePage = "<div class='bgimg w3-display-container w3-opacity-min' id='home'>"
+                       "<div class='w3-display-middle' style='white-space:nowrap;'>"
+                       "<form class='w3-container' action='/updateConfig' method='get'><h2>Cloud Config:</h2>"
+                       "<p><label>User ID (for this interface)</label><input class='w3-input w3-border w3-margin-bottom' type='text' name='userid' value='%USERID%' maxlength='20'></p>"
+                       "<p><label>Password </label><input class='w3-input w3-border w3-margin-bottom' type='password' name='stationpassword' value='%STATIONPASSWORD%'></p>"
+                       "<p><label>OTA Password </label><input class='w3-input w3-border w3-margin-bottom' type='password' name='otapassword' value='%OTAPASSWORD%'></p>"
+                       "<p><label>OpenWeatherMap API Key (get from <a href='https://openweathermap.org/' target='_BLANK'>here</a>)</label><input class='w3-input w3-border w3-margin-bottom' type='text' name='openWeatherMapApiKey' value='%WEATHERKEY%' maxlength='60'></p>"
+                       "<p><label>City Code (get from <a href='https://openweathermap.org/find?q=' target='_BLANK'>here</a>)</label><input class='w3-input w3-border w3-margin-bottom' type='text' name='city1' value='%CITY%' onkeypress='return isNumberKey(event)'></p>"
+                       "<button class='w3-button w3-block w3-grey w3-section w3-padding' type='submit'>Save</button>"
+                       "</form>"
+                       "</div>"
+                       "</div>";
+
+String parseConfigurePage() {
+  return header + configurePage + footer;
+}
+
+String parseHomePage() {
+  return header + homePage + footer;
+}
--- src/IOT-Mechanical-Alarm-Clock/IOT-Mechanical-Alarm-Clock.ino
+++ src/IOT-Mechanical-Alarm-Clock/IOT-Mechanical-Alarm-Clock.ino
@@ -2,6 +2,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiManager.h>
+#include <WiFiUdp.h>
+#include <Adafruit_NeoPixel.h>
 #include "FS.h"
 #include "SSD1306Wire.h"
 #include "OLEDDisplayUi.h"
@@ -12,6 +14,10 @@
 #define HOSTNAME "IOT-ALARM-CLOCK"
 #define CONFIG "/conf.txt"
 #define WEBSERVER_PORT 80
+#define NUMPIXELS 300 //change to however many are used
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
 float timeZone = 0.0;
 int externalLight = LED_BUILTIN;
@@ -25,6 +31,9 @@ const int buttonPin = D7;
 const int I2C_DISPLAY_ADDRESS = 0x3c; // I2C Address of your Display (usually 0x3c or 0x3d)
 const int SDA_PIN = D2;
 const int SCL_PIN = D5;
+const int neopixelPin =  D4;
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, neopixelPin, NEO_GRB + NEO_KHZ800);
 
 SSD1306Wire display(I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN);
 OLEDDisplayUi   ui( &display );
@@ -42,12 +51,279 @@ void handleUpdateConfigure();
 void handleNotFound();
 void handleRoot();
 void handleConfigure();
-void handleConfigureNoPassword();
+void initializePixels();
+uint32_t Wheel(byte WheelPos);
+void rainbowCycle();
+
+int rainbowCycleLoop0 = 0;
 
 void setup() {
+  Serial.begin(115200);
+  SPIFFS.begin();
+  delay(10);
 
+  Serial.println();
+  pinMode(externalLight, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+  readSettings();
+
+  //initialize display
+  display.init();
+  if (INVERT_DISPLAY) {
+    display.flipScreenVertically(); // connections at top of OLED display
+  }
+
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 10, "Connect to WiFi Network");
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 24, HOSTNAME);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 38, "And log into your home");
+  display.drawString(64, 48, "WiFi network to setup");
+  display.display();
+
+  //add initialize statements for website and the neopixels
+  parseHomePage();
+  parseConfigurePage();
+  initializePixels();
+
+  WiFiManager wifiManager;
+
+  String hostname(HOSTNAME);
+  if (!wifiManager.autoConnect((const char *)hostname.c_str())) {// new addition
+    delay(3000);
+    WiFi.disconnect(true);
+    ESP.reset();
+    delay(5000);
+  }
+
+  Serial.println("WEBSERVER_ENABLED");
+  //add server.on(); statements for webserver
+  server.on("/Home", HTTP_GET, handleRoot);
+  server.on("/Configure", handleConfigure);
+  server.on("/updateConfig", handleUpdateConfigure);
+  server.on("/FactoryReset", handleSystemReset);
+  server.on("/WifiReset", handleWifiReset);
+  server.onNotFound(handleRoot);
+  server.begin();
+  Serial.println("Server Started");
+  String webAddress = "http://" + WiFi.localIP().toString() + ":" + String(WEBSERVER_PORT) + "/";
+  Serial.println("Use this URL : " + webAddress);
+
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 10, "Web Interface On");
+  display.drawString(64, 20, "You May Connect to IP");
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(64, 30, WiFi.localIP().toString());
+  display.drawString(64, 46, "Port: " + String(WEBSERVER_PORT));
+  display.display();
+
+  timeClient.begin();
 }
 
 void loop() {
+  server.handleClient();
 
+  delay(1);
+
+  if (displayOn == true) {
+    displayOn = false;
+    previousMillisDisplay = currentMillis;
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_10);
+    display.drawString(64, 10, "Web Interface On");
+    display.drawString(64, 20, "You May Connect to IP");
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 30, WiFi.localIP().toString());
+    display.drawString(64, 46, "Port: " + String(WEBSERVER_PORT));
+    display.display();
+  }
+
+  if (currentMillis - previousMillisDisplay > intervalDisplay) {
+    display.clear();
+    display.display();
+  }
+
+  if (digitalRead(buttonPin) == LOW) {
+    displayOn = true;
+  }
+
+  rainbowCycle();
+}
+
+void handleSystemReset() {
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
+  }
+  Serial.println("Reset System Configuration");
+  if (SPIFFS.remove(CONFIG)) {
+    handleRoot();;
+    ESP.restart();
+  }
+}
+
+void handleWifiReset() {
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
+  }
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  handleRoot();
+  WiFiManager wifiManager;
+  wifiManager.resetSettings();
+  ESP.restart();
+}
+
+void writeSettings() { //editthis
+  // Save decoded message to SPIFFS file for playback on power up.
+  File f = SPIFFS.open(CONFIG, "w");
+  if (!f) {
+    Serial.println("File open failed!");
+  } else {
+    Serial.println("Saving settings now...");
+    f.println("www_username=" + String(www_username));
+    f.println("www_password=" + String(www_password));
+    f.println("weatherKey=" + WeatherApiKey);
+    f.println("CityID=" + String(CityID));
+    f.println("isMetric=" + String(IS_METRIC));
+  }
+  f.close();
+  readSettings();
+}
+
+void readSettings() { //editthis
+  if (SPIFFS.exists(CONFIG) == false) {
+    Serial.println("Settings File does not yet exists.");
+    writeSettings();
+    return;
+  }
+  File fr = SPIFFS.open(CONFIG, "r");
+  String line;
+  while (fr.available()) {
+    line = fr.readStringUntil('\n');
+
+    if (line.indexOf("www_username=") >= 0) {
+      String temp = line.substring(line.lastIndexOf("www_username=") + 13);
+      temp.trim();
+      temp.toCharArray(www_username, sizeof(temp));
+      Serial.println("www_username=" + String(www_username));
+    }
+    if (line.indexOf("www_password=") >= 0) {
+      String temp = line.substring(line.lastIndexOf("www_password=") + 13);
+      temp.trim();
+      temp.toCharArray(www_password, sizeof(temp));
+      Serial.println("www_password=" + String(www_password));
+    }
+    if (line.indexOf("weatherKey=") >= 0) {
+      WeatherApiKey = line.substring(line.lastIndexOf("weatherKey=") + 11);
+      WeatherApiKey.trim();
+      Serial.println("WeatherApiKey=" + WeatherApiKey);
+    }
+    if (line.indexOf("CityID=") >= 0) {
+      CityID = line.substring(line.lastIndexOf("CityID=") + 7).toInt();
+      Serial.println("CityID: " + String(CityID));
+    }
+    if (line.indexOf("isMetric=") >= 0) {
+      IS_METRIC = line.substring(line.lastIndexOf("isMetric=") + 9).toInt();
+      Serial.println("IS_METRIC=" + String(IS_METRIC));
+    }
+  }
+  fr.close();
+  weatherClient.updateWeatherApiKey(WeatherApiKey);
+  weatherClient.updateCityId(CityID);
+  weatherClient.setMetric(IS_METRIC);
+  weatherClient.updateCityId(CityID);
+}
+
+void handleUpdateConfigure() { //editthis
+  if (!server.authenticate(www_username, www_password)) {
+    return server.requestAuthentication();
+  }
+
+  String temp = server.arg("userid");
+  temp.toCharArray(www_username, sizeof(temp));
+  temp = server.arg("stationpassword");
+  temp.toCharArray(www_password, sizeof(temp));
+  OTA_Password = server.arg("otapassword");
+  WeatherApiKey = server.arg("openWeatherMapApiKey");
+  CityID = server.arg("city1").toInt();
+  IS_METRIC = server.hasArg("metric");
+
+  writeSettings();
+  handleConfigure();
+}
+
+void handleNotFound() {
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URL in the request
+}
+
+void handleRoot() {
+  String form = parseHomePage();
+  server.send(200, "text/html", form);  // Home webpage for the cloud
+}
+
+
+
+// converts the dBm to a range between 0 and 100%
+int8_t getWifiQuality() {
+  int32_t dbm = WiFi.RSSI();
+  if (dbm <= -100) {
+    return 0;
+  } else if (dbm >= -50) {
+    return 100;
+  } else {
+    return 2 * (dbm + 100);
+  }
+}
+
+void handleConfigure() { //editthis
+  String form = parseConfigurePage();
+  form.replace("%USERID%", www_username);
+  form.replace("%STATIONPASSWORD%", www_password);
+  form.replace("%OTAPASSWORD%", OTA_Password);
+  form.replace("%WEATHERKEY%", WeatherApiKey);
+  form.replace("%CITY%", String(CityID));
+
+  Serial.println(String(CityID));
+  server.send(200, "text/html", form);  // Configure portal for the cloud
+}
+
+void initializePixels() {
+  pixels.begin();
+  pixels.show();
+  //  pixels.setBrightness(50);
+}
+
+void rainbowCycle() {
+  if (rainbowCycleLoop0 < 256) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillisArray[0] > 1) {
+      previousMillisArray[0] = currentMillis;
+      for (int i = 0; i < pixels.numPixels(); i++) {
+        pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + rainbowCycleLoop0) & 255));
+      }
+      pixels.show();
+    }
+    rainbowCycleLoop0++;
+  } else if (rainbowCycleLoop0 >= 256) {
+    rainbowCycleLoop0 = 0;
+  }
+}
+
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
